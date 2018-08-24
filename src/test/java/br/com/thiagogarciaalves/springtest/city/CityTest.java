5450521a275409562d2bfb6ad9c4a21518edd5d1
Thiago G. Alves <thiago.alves@icolabora.com.br>
2018-08-24 17:12:17 -0300

    Testes com TestContainers

--- src/test/java/br/com/thiagogarciaalves/springtest/city/CityTest.java
+++ src/test/java/br/com/thiagogarciaalves/springtest/city/CityTest.java
@@ -1,16 +1,21 @@
 package br.com.thiagogarciaalves.springtest.city;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class CityTest {
+
+    @ClassRule
+    public static MySQLContainer mysql = new MySQLContainer("mysql:5.7");
 
     @Autowired
     private CityRepository cityRepo;
--- src/test/java/br/com/thiagogarciaalves/springtest/customer/CustomerTest.java
+++ src/test/java/br/com/thiagogarciaalves/springtest/customer/CustomerTest.java
@@ -1,14 +1,19 @@
 package br.com.thiagogarciaalves.springtest.customer;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MySQLContainer;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 public class CustomerTest {
+
+    @ClassRule
+    public static MySQLContainer mysql = new MySQLContainer("mysql:5.7");
 
     @Autowired
     private CustomerRepository repository;
